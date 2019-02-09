c0707f04923f40b2ece5948410480228aa24b5b5
SuperDru <30354748+SuperDru@users.noreply.github.com>
2019-02-10 02:14:09 +0300

    migrations update

---
+++ UsersManager/UsersManager/Migrations/20190209224217_TMigr7.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UsersManager.Migrations
+{
+    public partial class TMigr7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "salary_rate_requests",
+                keyColumns: new[] { "guid", "updated_at" },
+                keyValues: new object[] { "81305a7e-d967-41c7-8b57-bb87df2452ae", new DateTime(2017, 5, 8, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.DeleteData(
+                table: "salary_rate_requests",
+                keyColumns: new[] { "guid", "updated_at" },
+                keyValues: new object[] { "81305a7e-d967-41c7-8b57-bb87df2452ae", new DateTime(2017, 5, 11, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "guid",
+                table: "users",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "guid",
+                table: "salary_rate_requests",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.InsertData(
+                table: "salary_rate_requests",
+                columns: new[] { "guid", "updated_at", "explanation", "manager_id", "private_explanation", "rate", "reasons", "status", "user_id" },
+                values: new object[,]
+                {
+                    { new Guid("9f034ae5-f282-46d5-a359-78b15b82397d"), new DateTime(2017, 5, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "You haven't enough experience.", 2, "He's way too dumb.", 1500, "Just so", 3, 1 },
+                    { new Guid("9f034ae5-f282-46d5-a359-78b15b82397d"), new DateTime(2017, 5, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), null, 2, null, 1500, "Just so", 1, 1 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "guid",
+                value: new Guid("a2bb9c11-b5ac-4c1d-9c04-8f025b662fbd"));
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "guid",
+                value: new Guid("2e6ccbd4-86d7-499a-9d37-b00775453dc2"));
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "id",
+                keyValue: 3,
+                column: "guid",
+                value: new Guid("8e74485c-ab9b-4843-8103-1ce94d71196f"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_nick_name",
+                table: "users",
+                column: "nick_name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_email",
+                table: "users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_users_nick_name",
+                table: "users");
+
+            migrationBuilder.DeleteData(
+                table: "salary_rate_requests",
+                keyColumns: new[] { "guid", "updated_at" },
+                keyValues: new object[] { new Guid("9f034ae5-f282-46d5-a359-78b15b82397d"), new DateTime(2017, 5, 8, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.DeleteData(
+                table: "salary_rate_requests",
+                keyColumns: new[] { "guid", "updated_at" },
+                keyValues: new object[] { new Guid("9f034ae5-f282-46d5-a359-78b15b82397d"), new DateTime(2017, 5, 11, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.AlterColumn<string>(
+                name: "guid",
+                table: "users",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "guid",
+                table: "salary_rate_requests",
+                nullable: false,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.InsertData(
+                table: "salary_rate_requests",
+                columns: new[] { "guid", "updated_at", "explanation", "manager_id", "private_explanation", "rate", "reasons", "status", "user_id" },
+                values: new object[,]
+                {
+                    { "81305a7e-d967-41c7-8b57-bb87df2452ae", new DateTime(2017, 5, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "You haven't enough experience.", 2, "He's way too dumb.", 1500, "Just so", 3, 1 },
+                    { "81305a7e-d967-41c7-8b57-bb87df2452ae", new DateTime(2017, 5, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), null, 2, null, 1500, "Just so", 1, 1 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "guid",
+                value: "7b575d32-c324-4d06-87b4-5b2438bca565");
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "guid",
+                value: "4fb52256-dd39-4045-82cf-df93d1dd7647");
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "id",
+                keyValue: 3,
+                column: "guid",
+                value: "bb6ec5f3-3899-427d-be1e-94de6c737c21");
+        }
+    }
+}
